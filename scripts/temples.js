672fa531a3ef01faf23ac9bed6d9deca11ec5f7a
orson98 <carlosperaltamarreros@gmail.com>
2024-05-13 21:18:15 -0500

    new

---
+++ scripts/temples.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const hamburgerIcon = document.querySelector('.hamburger-icon');
+    const closeIcon = document.querySelector('.close-icon');
+    const navVisible = document.querySelector('.nav-visible');
+    const navHidden = document.querySelector('.nav-hidden');
+    
+    
+    hamburgerIcon.addEventListener('click', function() {
+        navVisible.classList.toggle('nav-hidden');
+    });
+ 
+    closeIcon.addEventListener('click', function() {
+        navVisible.classList.toggle('nav-hidden');
+    });
+});
